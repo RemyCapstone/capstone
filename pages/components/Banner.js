932e4795112f6b369f481b0e7f3df9a261498955
Khandaker Shayel <kshayel1717@bths.edu>
2022-03-03 18:11:52 -0500

    Added banner component
    Basic @Chakra-UI/React setup that gives an initial banner UI that gives users options to browse either rental or for sale units

---
+++ pages/components/Banner.js
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import Image from "next/image";
+import { Flex, Box, Text, Button } from "@chakra-ui/react";
+
+const Banner = (props) => {
+  const {purpose, imageURL, title1, title2, desc1, desc2, linkName, buttonText} = props;
+
+  return(
+    <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
+      <Image width={500} height={300} src={imageURL} alt="banner"/>
+      <Box p="5">
+        <Text color="gray.500" fontSize="sm" fontWeight="medium">{purpose}</Text>
+        <Text  fontSize="3xl" fontWeight="bold">
+          {title1}
+          <br />
+          {title2}
+        </Text>
+        <Text color="gray.700" fontSize="lg" paddingTop="3" paddingBottom="3">
+          {desc1}
+          <br />
+          {desc2}
+        </Text>
+        <Button fontSize="xl">
+          <Link href={linkName}>{buttonText}</Link>
+        </Button>
+      </Box>
+    </Flex>
+  )
+};
+
+export default Banner;
