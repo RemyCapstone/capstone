8dbfbf8d147b3e8c015dd32ac74b515910c962a0
Khandaker Shayel <kshayel1717@bths.edu>
2022-03-06 20:42:22 -0500

    search bar basics

--- pages/search.js
+++ pages/search.js
@@ -5,6 +5,8 @@ import { Flex, Box, Text, Icon } from '@chakra-ui/react';
 import { BsFilter } from 'react-icons/bs';
 
 import Property from '../components/Property';
+import SearchFilters from '../components/SearchFilter';
+import noresult from '../assets/images/noresults.png'
 
 const SearchPage = ({properties}) => {
     const [searchFilters, setSearchFilters] = useState(false);
@@ -13,11 +15,32 @@ const SearchPage = ({properties}) => {
     return (
         <Box>
             <Flex  onClick={() => setSearchFilters(!searchFilters)} cursor='pointer' bg='gray.100' borderBottom='1px' borderColor='gray.200' p='2' fontWeight='black' fontSize='lg' justifyContent='center' alignItems='center'>
-                <Text>Narrow Your Search!</Text>
+                <Text>Search By Filters</Text>
                 <Icon paddingLeft='2' w='7' as={BsFilter} />
             </Flex>
+            {searchFilters && <SearchFilters />}
+            <Text fontSize='2xl' p='4' fontWeight='bold'>
+                Properties {router.query.purpose}
+            </Text>
+             <Flex flexWrap='wrap'>
+                {/*properties.map((property) => <Property property={property} key={property.zpid} />) */}
+            </Flex>
+            {properties.length === 0 && (
+            <Flex justifyContent='center' alignItems='center' flexDir='column' marginTop='5' marginBottom='5'>
+            <Image src={noresult} />
+            <Text fontSize='xl' marginTop='3'>No Result Found.</Text>
+            </Flex>
+            )}
         </Box>
     )
+}
+
+export async function getServerSideProps({ query }) {
+  return {
+    props: {
+      properties: [],
+    },
+  };
 }
 
 export default SearchPage;
