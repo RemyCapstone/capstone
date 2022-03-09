c958e334dfad15900563c932942faf6df912bed4
Khandaker Shayel <kshayel1717@bths.edu>
2022-03-08 22:05:05 -0500

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -29,23 +29,29 @@ const AboutPage = () => {
             <Grid templateColumns='repeat(3, 1fr)' gap={6}>
                 <GridItem w='100%' h='60' bg='gray.100' borderRadius='10'>
                     <Box p='3'>
-                        <Icon as={FcBarChart} w={50} h={50} />
-                    <Text fontSize='2xl' fontWeight='bold' textAlign='center' color='gray.700' p='2'>Provide real data</Text>
-                    <Text textAlign='center' color='gray.600'>We provide real estate data along with details of their past and current violations by incorporating HPD data and 311 complaints.</Text>
+                        <Flex justifyContent="center" alignItems="center">
+                            <Icon as={FcBarChart} w={50} h={50} />
+                        </Flex>
+                        <Text fontSize='2xl' fontWeight='bold' textAlign='center' color='gray.700' p='2'>Provide real data</Text>
+                        <Text textAlign='center' color='gray.600'>We provide real estate data along with details of their past and current violations by incorporating HPD data and 311 complaints.</Text>
                     </Box>
                 </GridItem>
                 <GridItem w='100%' h='60' bg='gray.100' borderRadius='10'>
                     <Box p='3'>
-                    <Icon as={FcInspection} w={50} h={50} />
-                    <Text fontSize='2xl' fontWeight='bold' textAlign='center' color='gray.700' p='2'>Hold landlords accountable</Text>
-                    <Text textAlign='center' color='gray.600'>We show violations and ratings to make sure landlords avoid negligence and provide proper living conditions for their rental units.</Text>
+                        <Flex justifyContent="center" alignItems="center">
+                            <Icon as={FcInspection} w={50} h={50} />
+                        </Flex>
+                        <Text fontSize='2xl' fontWeight='bold' textAlign='center' color='gray.700' p='2'>Hold landlords accountable</Text>
+                        <Text textAlign='center' color='gray.600'>We show violations and ratings to make sure landlords avoid negligence and provide proper living conditions for their rental units.</Text>
                     </Box>
                 </GridItem>
                 <GridItem w='100%' h='60' bg='gray.100' borderRadius='10'>
                     <Box p='3'>
-                    <Icon as={FcSearch} w={50} h={50} />
-                    <Text fontSize='2xl' fontWeight='bold' textAlign='center' color='gray.700' p='2'>Emphasize transparency</Text>
-                    <Text textAlign='center' color='gray.600'>We want apartment hunters to have all the information they need before deciding on their next home within one search.</Text>
+                        <Flex justifyContent="center" alignItems="center">
+                            <Icon as={FcSearch} w={50} h={50} />
+                        </Flex>
+                        <Text fontSize='2xl' fontWeight='bold' textAlign='center' color='gray.700' p='2'>Emphasize transparency</Text>
+                        <Text textAlign='center' color='gray.600'>We want apartment hunters to have all the information they need before deciding on their next home within one search.</Text>
                     </Box>
                 </GridItem>
             </Grid>
@@ -55,6 +61,7 @@ const AboutPage = () => {
                     Our Impact
                 </Text>
             </Flex>
+            <Flex justifyContent='center' alignItems='center' >
             <Grid
                 h='450px'
                 templateRows='repeat(3, 1fr)'
@@ -63,7 +70,7 @@ const AboutPage = () => {
             >
 
                 <GridItem rowSpan={3} colSpan={1}>
-                    <Icon as={FcHome} w={400} h={400} />
+                    <Icon as={FcHome} w={250} h={400} />
                 </GridItem>
                 <GridItem colSpan={2}>
                     <Text fontSize='4xl' fontWeight='bold' color='blue.500' p='3'>7,200 apartments</Text>
@@ -79,6 +86,7 @@ const AboutPage = () => {
                 </GridItem>
 
             </Grid>
+            </Flex>
         </Box>
 
 
