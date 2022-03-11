b54a528fd3489048ba189087461a103d94a493b3
Khandaker Shayel <kshayel1717@bths.edu>
2022-03-11 01:20:53 -0500

    Update LocationPin.js

--- components/Map/LocationPin.js
+++ components/Map/LocationPin.js
@@ -2,7 +2,7 @@ import { FaMapPin } from 'react-icons/fa';
 import { Box, Flex, Text} from "@chakra-ui/react";
 
 const LocationPin = ({ text }) => (
-        <div className="pin"  style={{ width: '100%' }}>
+        <div className="pin">
             <FaMapPin  className="pin-icon" size={20} color="red"/>
             
         </div>
