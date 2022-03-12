a646bf7f8bf7dc2225c107f6bc225a3155668790
Khandaker Shayel <kshayel1717@bths.edu>
2022-03-11 21:15:26 -0500

    pricehistorytable component created

---
+++ components/PriceHistory/PriceHistoryTable.js
@@ -0,0 +1,10 @@
+import { Table, Thead, Tbody, Tr, Th, Td, chakra } from '@chakra-ui/react'
+import { TriangleDownIcon, TriangleUpIcon } from '@chakra-ui/icons'
+import { useTable, useSortBy } from 'react-table'
+
+
+const PriceHistoryTable = () => {
+
+}
+
+export default PriceHistoryTable;
