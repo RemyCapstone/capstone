6dd414a49bfe929aa34f72b07be99b0b6ca59432
skeyan <eyan0749@gmail.com>
2022-03-13 15:43:52 -0400

    Add check for duplicate email with basic alert window

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -2,10 +2,10 @@ import { MongoClient } from 'mongodb';
 
 // api/signup
 // POST api/signup
-
 async function handler(req, res) {
     if (req.method === 'POST') {
         // contains: firstname, lastname, email, password
+        console.log("I AM HERE");
         const newUserData = req.body;
 
         const bcrypt = require ('bcrypt');
@@ -18,17 +18,26 @@ async function handler(req, res) {
             email: newUserData.email,
             password: passwordHash
         };
+
         const client = await MongoClient.connect('mongodb+srv://remycapstone:NSpBNwJpvKxRko6T@cluster0.hwcwt.mongodb.net/users?retryWrites=true&w=majority');
         const db = client.db();
 
         const usersCollection = db.collection('users');
+        const emailResult = await usersCollection.findOne({
+            email: submitUserData.email
+        })
+
+        if (emailResult)
+        {
+            res.status(401).json({message: 401});
+            client.close();
+            return;
+        }
+
         const result = await usersCollection.insertOne(submitUserData);
-
         console.log('Result of inserting:', result);
-
         client.close();
-
-        res.status(201).json({message: "User inserted!"});
+        res.status(201).json({message: 201});
     }
 }
 
--- pages/signup.js
+++ pages/signup.js
@@ -4,16 +4,25 @@ function SignupPage() {
     async function addUserHandler(enteredUserData) {
         console.log(enteredUserData);
 
-        const response = await fetch('/api/signup', {
-            method: 'POST',
-            body: JSON.stringify(enteredUserData),
-            headers: {
-                'Content-Type': 'application/json'
+        try {
+            const response = await fetch('/api/signup', {
+                method: 'POST',
+                body: JSON.stringify(enteredUserData),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            const data = await response.json();
+            if (data.message === 401)
+            {
+                alert("Email already exists.");
             }
-        });
-
-        const data = await response.json();
-        console.log(data);
+        }
+        catch
+        {
+            console.log("error")
+            alert("Error with internal API");
+        }
     }
 
     return <SignupForm onSignup={addUserHandler}></SignupForm>
