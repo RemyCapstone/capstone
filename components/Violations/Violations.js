82c092e30d06d9cd8b2d0ce25b6d8804b2c2d066
Khandaker Shayel <kshayel1717@bths.edu>
2022-03-19 18:11:15 -0400

    basic hpd info displayed

--- components/Violations/Violations.js
+++ components/Violations/Violations.js
@@ -3,11 +3,17 @@ import { Box, Flex, Spacer, Text } from '@chakra-ui/layout';
 import millify from 'millify';
 import {useState, useEffect} from 'react';
 import { violationOptions, plutoOptions, fetchOpenApi } from "../../utils/hpdViolations";
+import MiniTable from './MiniTable';
 
 const Violations = ({data, registered}) => {
     const [violationsData, setViolationsData] = useState([]);
     const [units, setUnits] = useState(0);
     //console.log(data)
+
+   
+
+    const openViolations = violationsData.filter(vio => vio.violationstatus === 'Open')
+    const avgVio = openViolations.length === 0 ? 0 : Math.round((openViolations.length / units.unitstotal) * 100) / 100
 
     useEffect(() => {
         if(data){
@@ -50,7 +56,7 @@ const Violations = ({data, registered}) => {
     }
 
     return(
-        <Box overflowY="auto" maxHeight="500px">
+        <Box overflowY="auto" maxHeight="500px" borderBottom='1px' borderColor='gray.300' paddingBottom={5}>
             <Table variant='simple' colorScheme='facebook'>
                      <Thead position="sticky" top={0} bgColor="white">
                         <Tr>
@@ -59,48 +65,14 @@ const Violations = ({data, registered}) => {
                     </Thead>
             </Table>
             <Flex>
-                <Box w='33%'>
-                    <Table variant='striped' colorScheme='teal'>
-                        <Thead position="sticky" top={0} bgColor="white">
-                            <Tr>
-                                <Th>HPD Building ID</Th>
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                            <Tr>
-                                <Td>{data.buildingid}</Td>
-                            </Tr>
-                        </Tbody>
-                </Table>
-                </Box>
-                <Box w='33%'>
-                    <Table variant='striped' colorScheme='blackAlpha'>
-                        <Thead position="sticky" top={0} bgColor="white">
-                            <Tr>
-                                <Th>Boro-Block-Lot (BBL)</Th>
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                            <Tr>
-                                <Td>{data.boro}-{data.block}-{data.lot}</Td>
-                            </Tr>
-                        </Tbody>
-                </Table>
-                </Box>
-                <Box w='33%'>
-                    <Table variant='striped' colorScheme='orange'>
-                        <Thead position="sticky" top={0} bgColor="white">
-                            <Tr>
-                                <Th>Total Residential Units</Th>
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                            <Tr>
-                                <Td>{units.unitstotal}</Td>
-                            </Tr>
-                        </Tbody>
-                </Table>
-                </Box>
+                <MiniTable title='HPD Building ID' color='teal' content={data.buildingid} tooltip='Unique identifier for a building registered with the HPD'/>
+                <MiniTable title='Boro-Block-Lot (BBL)' color='blackAlpha' content={`${data.boro}-${data.block}-${data.lot}`} tooltip='An indentifier used by the Department of Finance Tax Records and Primary Land Use Tax Lot Output'/>
+                <MiniTable title='Total Residential Units' color='twitter' content={`${units.unitstotal} units`} />
+            </Flex>
+            <Flex >
+                <MiniTable title='Total Violations' color='orange' content={`${violationsData.length} total HPD violations in this building`} height='40'/>
+                <MiniTable title='Current Open Violations' color='red' content={`${openViolations.length} HPD violations are open. Average: ${avgVio} violations/ unit`} height='40' tooltip='Open violations are ones that have yet to be fixed. Average is calculated using the total residential units, naturally more units will have more complaints so average is a better number to use.'/>
+                <MiniTable title='Landlord/ Owner' color='purple' content={units.ownername} height='40' tooltip='Most common name associated with the building'/>
             </Flex>
         </Box>
     )
