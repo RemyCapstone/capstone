dad131579af721b8e45e9d9ab603d666526f31a3
skeyan <eyan0749@gmail.com>
2022-04-08 11:54:07 -0400

    Add form details to email

--- pages/api/reportViolation.js
+++ pages/api/reportViolation.js
@@ -1,5 +1,13 @@
 const reportViolationHandler = async (req, res) => {
-    const {zpid, address, purpose, userEmail, additionalDetails} = req.body;
+    const {
+        zpid, address, purpose, userEmail, dateReported,
+        problem, problemDetail, locationDetail,
+        additionalDetails,
+        tenantFirstName, tenantLastName, tenantEmail,
+        tenantPrimaryPhone, tenantPhone,
+        ownerFirstName, ownerLastName, ownerPhone,
+        childLive, childVisit
+    } = req.body;
 
     let nodemailer = require('nodemailer')
     const transporter = nodemailer.createTransport({
@@ -18,11 +26,27 @@ const reportViolationHandler = async (req, res) => {
         text: 'Text',
         html: `
             <h1>Violation Reported</h1>
-            <h2>Details</h2>
+            <h2>Key Information</h2>
             <p><b>Zillow ID:</b> ${zpid}</p>
             <p><b>Address of Listing in Question:</b> ${address}</p>
             <p><b>Type of Listing:</b> ${purpose}</p>
             <p><b>Reported by User:</b> ${userEmail}</p>
+            <p><b>Reported At:</b> ${dateReported}</p>
+            <br/>
+            <h2>Report Details</h2>
+            <p><b>Problem:</b> ${problem}</p>
+            <p><b>Problem Detail:</b> ${problemDetail}</p>
+            <p><b>Location Detail:</b> ${locationDetail}</p>
+            <p><b>Tenant First Name:</b> ${tenantFirstName}</p>
+            <p><b>Tenant Last Name:</b> ${tenantLastName}</p>
+            <p><b>Tenant Email:</b> ${tenantEmail}</p>
+            <p><b>Tenant Primary Phone:</b> ${tenantPrimaryPhone}</p>
+            <p><b>Tenant Phone:</b> ${tenantPhone}</p>
+            <p><b>Owner First Name:</b> ${ownerFirstName}</p>
+            <p><b>Owner Last Name:</b> ${ownerLastName}</p>
+            <p><b>Owner Phone:</b> ${ownerPhone}</p>
+            <p><b>Does a child under six live here?:</b> ${childLive}</p>
+            <p><b>Does a child under six regularly visit here?:</b> ${childVisit}</p>
             <br/>
             <h2>Additional Details</h2>
             <p>${additionalDetails}</p>
