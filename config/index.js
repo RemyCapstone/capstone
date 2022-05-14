b41c2949435cb2c01a3cca4444a79b58256937a1
skeyan <eyan0749@gmail.com>
2022-05-14 18:49:33 -0400

    Use next_public for env

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
-const dev = process.env.MY_CURRENT_PLATFORM !== 'prod';
-console.log("here in config/index.js", process.env.GMAIL_PASSWORD, process.env.MY_CURRENT_PLATFORM, process.env.PLATFORM, process.env.VERCEL_ENV);
+const dev = process.env.NEXT_PUBLIC_PLATFORM !== 'prod';
+console.log("here in config/index.js", process.env.NEXT_PUBLIC_PLATFORM);
 
 export const server = dev ? 'http://localhost:3000' : 'https://remy-nyc.vercel.app';
