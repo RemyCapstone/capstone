ff605d5baba3fe6f89f337172096561415ba6645
skeyan <eyan0749@gmail.com>
2022-05-14 18:44:23 -0400

    Debugging

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
-const dev = process.env.NEXT_PUBLIC_VERCEL_ENV !== 'prod';
-console.log("here in config/index.js", process.env.PLATFORM);
+const dev = process.env.VERCEL_ENV !== 'production';
+console.log("here in config/index.js", process.env.PLATFORM, process.env.VERCEL_ENV);
 
 export const server = dev ? 'http://localhost:3000' : 'https://remy-nyc.vercel.app';
